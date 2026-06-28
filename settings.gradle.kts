f8870285bb86d7b8de9e305b88487b886ddca6ba
Hogwai <51323232+Hogwai@users.noreply.github.com>
2026-06-28 23:45:00 +0200

    feat: remove demo from the lib, add link in README.md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,12 @@
 rootProject.name = "dynamodb-simplified"
 
-include(
-    "dynamodb-simplified-core",
-    "dynamodb-simplified-demo"
-)
+include("dynamodb-simplified-core")
 
 pluginManagement {
     val props = java.util.Properties()
     props.load(java.io.FileInputStream("gradle.properties"))
     plugins {
         id("net.ltgt.errorprone") version props.getProperty("versionErrorpronePlugin")
-        id("io.micronaut.application") version props.getProperty("versionMicronautPlugins")
-        id("io.micronaut.minimal.application") version props.getProperty("versionMicronautPlugins")
-        id("com.gradleup.shadow") version props.getProperty("versionShadow")
-        id("io.micronaut.aot") version props.getProperty("versionMicronautPlugins")
         id("org.jreleaser") version props.getProperty("versionJreleaser")
     }
 }
